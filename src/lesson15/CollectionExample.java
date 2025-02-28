0b22b68de96f963687f5ac20338bf643b5efa867
Olyamyr <troshina1994@bk.ru>
2025-02-28 19:28:09 +0300

    Изменения

--- src/lesson15/CollectionExample.java
+++ src/lesson15/CollectionExample.java
@@ -1,4 +1,32 @@
 package lesson15;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class CollectionExample {
+    public static void main(String[] args) {
+        CollectionUtilsImpl utils = new CollectionUtilsImpl();
+
+        //Создаем две коллекции
+        List<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(5);
+
+        List<Integer> linkedList = new LinkedList<>();
+        linkedList.add(1);
+        linkedList.add(5);
+        linkedList.add(2);
+        linkedList.add(4);
+        linkedList.add(9);
+
+        System.out.println("Объединение двух коллекций с дубликатами: " + utils.union(arrayList, linkedList));
+        System.out.println("Пересечение двух коллекций с дубликатами: " + utils.intersection(arrayList, linkedList));
+        System.out.println("Объединение двух коллекций без дубликатов: " + utils.unionWithoutDuplicate(arrayList, linkedList));
+        System.out.println("Пересечение двух коллекций без дубликатов: " + utils.intersectionWithoutDuplicate(arrayList, linkedList));
+        System.out.println("Разность двух коллекций: " + utils.difference(arrayList, linkedList));
+    }
 }
--- src/lesson15/CollectionUtilsImpl.java
+++ src/lesson15/CollectionUtilsImpl.java
@@ -1,4 +1,55 @@
 package lesson15;
 
-public class CollectionUtilsImpl {
+import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class CollectionUtilsImpl implements CollectionUtils {
+    @Override
+    public Collection<Integer> union(Collection<Integer> a, Collection<Integer> b) throws NullPointerException {
+        //Вариант 1 - передаем в конструктор 1 коллекцию и отдельно добавляем другую
+        Collection<Integer> result = new ArrayList<>(a);
+        result.addAll(b);
+        return result;
+
+        //Вариант 2 - создаем пустую коллекцию и отдельно добавляем остальные две
+//        Collection<Integer> result = new ArrayList<>();
+//        result.addAll(a);
+//        result.addAll(b);
+//        return result;
+    }
+
+    @Override
+    public Collection<Integer> intersection(Collection<Integer> a, Collection<Integer> b) throws NullPointerException {
+        Collection<Integer> result = new ArrayList<>(a);
+        result.retainAll(b);
+        return result;
+    }
+
+    @Override
+    public Set<Integer> unionWithoutDuplicate(Collection<Integer> a, Collection<Integer> b) throws NullPointerException {
+        Set<Integer> result = new HashSet<>();
+        result.addAll(a);
+        result.addAll(b);
+
+        return result;
+    }
+
+    @Override
+    public Set<Integer> intersectionWithoutDuplicate(Collection<Integer> a, Collection<Integer> b) throws NullPointerException {
+        Set<Integer> result = new HashSet<>();
+        for (Integer element : a) {
+            if (b.contains(element)) {
+                result.add(element);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Collection<Integer> difference(Collection<Integer> a, Collection<Integer> b) throws NullPointerException {
+        Collection<Integer> result = new ArrayList<>();
+        result.removeAll(b);
+        return result;
+    }
 }
